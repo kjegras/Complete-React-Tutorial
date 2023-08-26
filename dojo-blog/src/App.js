c3b67147bb40e7cf8ea2cce60f82dfbed91ef735
kjegras <kjegras@gmail.com>
2023-08-26 16:10:28 +0200

    Forgot this one

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,34 +1,52 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
-import NotFound from './NotFound';
+import Navbar from './Navbar'
+import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Create from './Create'
+import Edit from './Edit'
+import BlogDetails from './BlogDetails'
+import NotFound from './NotFound'
+
+import { useState } from 'react'
 
 function App() {
+  const [currentBlog, setCurrentBlog] = useState({})
+
+  const retrieveCurrentBlog = () => {
+    return currentBlog
+  }
+
+  const storeCurrentBlog = (b) => {
+    setCurrentBlog(b)
+    console.log('App.storeCurrentBlog')
+    console.log(b)
+  }
+
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Navbar />
-        <div className="content">
+        <div className='content'>
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <Home />
             </Route>
-            <Route path="/create">
+            <Route path='/create'>
               <Create />
             </Route>
-            <Route path="/blogs/:id">
-              <BlogDetails />
+            <Route path='/edit'>
+              <Edit retrieveCurrentBlog={retrieveCurrentBlog} />
             </Route>
-            <Route path="*">
+            <Route path='/blogs/:id'>
+              <BlogDetails storeCurrentBlog={storeCurrentBlog} />
+            </Route>
+            <Route path='*'>
               <NotFound />
             </Route>
           </Switch>
         </div>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
